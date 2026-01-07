048ac9c2bce4eb1cda86d5a994985ed745156169
gdetter <gabedetter@gmail.com>
2026-01-06 20:53:08 -0500

    bug

--- stepper_test.py
+++ stepper_test.py
@@ -28,7 +28,7 @@ motor1 = StepperDriver(dir=PIN27, stp=PIN29, slp = PIN31, rst = PIN33, ms3 = PIN
 
 #Configure Steppers
 motor1.ratio = 12
-motor1.microstep = 4
+motor1.microstep = 8
 motor1.speed = 30
 # motor2.ratio = 3.6
 # motor2.microstep = 16
